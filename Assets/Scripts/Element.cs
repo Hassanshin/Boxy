1d529e8d094d52c32e51fe1d7131ebba2124fbae
Hassanshin <f3fahmi@gmail.com>
2019-07-26 16:24:43 +0700

    shuffle script

---
+++ Assets/Scripts/Element.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Element : MonoBehaviour
+{
+    [SerializeField]
+    private ElementType m_ElementType;
+
+    [SerializeField]
+    private int m_Tier;
+
+    [SerializeField]
+    private int m_Emotion;
+
+    public void Start()
+    {
+        
+    }
+}
+
+public enum ElementType
+{
+    fire, water, air, earth
+}
+
---
+++ Assets/Scripts/ElementManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class ElementManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> monstersPoolList = new List<GameObject>();
+
+    [SerializeField]
+    private List<GameObject> shuffledList = new List<GameObject>();
+
+    private void Start()
+    {
+        shuffledList = ShuffleList(monstersPoolList);
+    }
+
+    public List<GameObject> ShuffleList<GameObject>(List<GameObject> inputList)
+    {
+        List<GameObject> randomList = new List<GameObject>();
+
+        //Random r = new Random();
+        int randomIndex = 0;
+        while (inputList.Count > 0)
+        {
+            randomIndex = Random.Range(0, inputList.Count); //Choose a random object in the list
+            randomList.Add(inputList[randomIndex]); //add it to the new, random list
+            inputList.RemoveAt(randomIndex); //remove to avoid duplicates
+        }
+
+        return randomList; //return the new random list
+    }
+}
